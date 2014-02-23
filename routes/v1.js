b1a6d27d3e5b3844251dde6f55aee6463dd9f0d7
Cam Pedersen <diffference@gmail.com>
2014-02-22 18:49:50 -0600

    add specific loan/person routes

--- routes/v1.js
+++ routes/v1.js
@@ -64,3 +64,73 @@ app.get('/v1/loans', function (req, res) {
     });
   });
 });
+
+app.get('/v1/person/:id', function (req, res) {
+  var id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+    return res.send({
+      success: false,
+      error: 'Invalid id'
+    });
+  }
+
+  var query = 'select * from people where person_id = $1';
+  var values = [ id ];
+  app.db.query(query, values, function (err, results) {
+    if (err) {
+      console.log(err);
+      return res.send({
+        success: false,
+        error: 'Database error'
+      });
+    }
+
+    if (!results.length) {
+      return res.send({
+        success: false,
+        error: 'Person does not exist'
+      });
+    }
+
+    res.send({
+      success: true,
+      person: results[0]
+    });
+  });
+});
+
+app.get('/v1/loan/:id', function (req, res) {
+  var id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+    return res.send({
+      success: false,
+      error: 'Invalid id'
+    });
+  }
+
+  var query = 'select * from loans where loan_id = $1';
+  var values = [ id ];
+  app.db.query(query, values, function (err, results) {
+    if (err) {
+      console.log(err);
+      return res.send({
+        success: false,
+        error: 'Database error'
+      });
+    }
+
+    if (!results.length) {
+      return res.send({
+        success: false,
+        error: 'Loan does not exist'
+      });
+    }
+
+    res.send({
+      success: true,
+      loan: results[0]
+    });
+  });
+});
